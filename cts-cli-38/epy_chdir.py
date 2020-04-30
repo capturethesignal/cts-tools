c4457643c6b10fe8bf474c8e05a7000687c43ade
Clayton Smith <argilo@gmail.com>
2020-04-29 21:46:39 -0400

    Add cts-cli for GNU Radio 3.8

---
+++ cts-cli-38/epy_chdir.py
@@ -0,0 +1,6 @@
+
+import os
+
+script_path = os.path.dirname(os.path.realpath(__file__))
+os.chdir(script_path)
+
---
+++ cts-cli-38/epy_mkfifo.py
@@ -0,0 +1,15 @@
+import os
+
+path = os.path.join(os.path.expanduser('~'), "cts.fifo")
+
+if not os.path.exists(path):
+    try:
+        os.mkfifo(path)
+    except OSError as e:
+        print("Cannot create FIFO '{}': {}".format(path, e))
+    else:
+        print("Created FIFO: {}".format(path))
+        print("IMPORTANT: Before you can see the source sample rate, you must start consuming samples from the FIFO")
+else:
+    print("FIFO already exists (not creating): {}".format(path))
+    print("IMPORTANT: Before you can see the source sample rate, you must start consuming samples from the FIFO")
